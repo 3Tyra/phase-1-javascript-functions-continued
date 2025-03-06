efb57d6e56406b9851769dcd71834d735d284c83
Tyra <tyra.mwai@student.moringaschool.com>
2025-03-06 22:00:23 +0300

    Added Changes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun()); 
+  console.log(saturdayFun("paint a masterpiece"));
+  
+  
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  console.log(mondayWork()); 
+  console.log(mondayWork("work from home")); 
+  
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  
+  console.log(encouragingPromptFunction("a dedicated programmer")); 
+  console.log(wrapAdjective("%")("a dedicated programmer"));
+  
+  
